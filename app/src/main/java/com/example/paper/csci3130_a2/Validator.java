23c8538a86bc4b136fc2f9063ae24baf2559f258
jw670207 <35313099+jw670207@users.noreply.github.com>
2018-06-05 11:58:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/paper/csci3130_a2/Validator.java
@@ -0,0 +1,12 @@
+package com.example.paper.csci3130_a2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Validator extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
